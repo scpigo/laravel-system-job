6a861cf5ee5a6fb4c6a9a6dd85ec0ab587e03781
maxim <maximiliyan99@mail.ru>
2022-03-23 11:36:06 +0300

    changed composer.json file

--- src/Dto/SystemJobSchedulerDto.php
+++ src/Dto/SystemJobSchedulerDto.php
@@ -6,6 +6,6 @@ use Illuminate\Support\Carbon;
 
 class SystemJobSchedulerDto {
     public string $action;
-    public string $action_params;
+    public string $action_params = '';
     public Carbon $scheduled_at;
 }
